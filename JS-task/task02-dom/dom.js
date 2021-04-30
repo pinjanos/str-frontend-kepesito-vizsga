6db9169edff5996938b7fd4c6d825c42dcf79fed
pinjanos <pin.janos@gmail.com>
2021-04-30 12:57:04 +0200

    végem van

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,19 +1,19 @@
 function changeOuterLinks() {
-    const navLinks = document.querySelectorAll(`nav#link-list a`)
+    const navLinks = document.querySelectorAll('nav#link-list a');
     navLinks.forEach( element => {
-        if(element.innerHTML.includes('outer-links')) {
+        if (element.innerHTML.includes('outer-link')) {
             element.setAttribute('target', '_blank');
             element.innerHTML = `<strong>${element.innerHTML}</strong>`;
         }
     })
 
-    document.querySelectorAll("nav").forEach((e) => {
-        e.style.display = "flex";
-        e.style.flexDirection = "column";
-        e.style.margin = "0px auto";
-        e.style.width = "30%";
-        e.style.border = "1px solid blue";
-        e.style.padding = "16px";
+    document.querySelectorAll("nav").forEach((element) => {
+        element.style.display = "flex";
+        element.style.flexDirection = "column";
+        element.style.margin = "0px auto";
+        element.style.width = "30%";
+        element.style.border = "1px solid blue";
+        element.style.padding = "16px";
     });
 }
 
--- angular-cinema-project/cinema/src/app/service/http.service.ts
+++ angular-cinema-project/cinema/src/app/service/http.service.ts
@@ -1,19 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Movie } from '../model/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  BASE_URL = 'http://localhost:3000/movies';
+  BASE_URL = 'https://tr360-frontend-exam-april.azurewebsites.net/pinjanos/movies';
 
-  constructor(undefined) { }
+  movieList: BehaviorSubject<Movie[]> = new BehaviorSubject<Movie[]>([])
 
-  getMovieList():any {
-    return null;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getMovieList(): void {
+    this.http.get<Movie[]>(`${this.BASE_URL}`).subscribe(
+      list => this.movieList.next(list)
+    );
   }
 
-  deleteMovie(id):any {
-    return null;
+  deleteMovie(id: number): void {
+    this.http.delete(`${this.BASE_URL}/${id}`).subscribe(
+      () => this.getMovieList()
+    )
   }
 }
