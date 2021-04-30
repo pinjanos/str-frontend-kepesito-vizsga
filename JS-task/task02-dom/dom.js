17675c35bf92e14dc107711ff7ddd6231384447d
pinjanos <pin.janos@gmail.com>
2021-04-30 10:47:39 +0200

    algorithm and dom done

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,23 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+    const navLinks = document.querySelectorAll(`nav#link-list a`)
+    navLinks.forEach( element => {
+        if(element.innerHTML.includes('outer-links')) {
+            element.setAttribute('target', '_blank');
+            element.innerHTML = `<strong>${element.innerHTML}</strong>`;
+        }
+    })
+
+    document.querySelectorAll("nav").forEach((e) => {
+        e.style.display = "flex";
+        e.style.flexDirection = "column";
+        e.style.margin = "0px auto";
+        e.style.width = "30%";
+        e.style.border = "1px solid blue";
+        e.style.padding = "16px";
+    });
+}
+
+
 
 
 export { changeOuterLinks };
